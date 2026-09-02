5476ff944be40a90424a6a023f805b9ae9ac2178
liveforpresent <dailynsteady@gmail.com>
2026-09-02 21:05:23 +0900

    init

---
+++ src/test/kotlin/com/staysupplierhub/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.staysupplierhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
